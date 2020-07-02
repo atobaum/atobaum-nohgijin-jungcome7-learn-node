ca3334d2598946261d9d677f9dd602d92e692a4d
Atobaum <atobaum@gmail.com>
2020-07-02 15:13:34 +0900

    Change folder structure
    move all files to debugging folder

--- about_debugging/index.js
+++ about_debugging/index.js
@@ -1,11 +1,5 @@
 var mul=require('./mulmodule')
 let addModule = require('./addModule');
-<<<<<<< HEAD
-const powerModule = require('./powerModule');
-let a = "asads";
-=======
-
->>>>>>> debugging
 console.log(mul.divModule(1,2))
 console.log(mul.multiModule(1,2))
 console.log("1 + 2 = ", addModule.add(1,2));
