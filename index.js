d37584dcff42660304622062d417b73fe18254f0
woowahan24 <woowahan24@MacBook-Pro.local>
2020-07-02 14:55:05 +0900

    step into and out

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var mul=require('./mulmodule')
 let addModule = require('./addModule');
 const powerModule = require('./powerModule');
-
+let a = "asads";
 console.log(mul.divModule(1,2))
 console.log(mul.multiModule(1,2))
 console.log("1 + 2 = ", addModule.add(1,2));
