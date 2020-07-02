b3fbdeb1090761cc20a93b8ec351436a2dcb9602
nohgijin <nohgijin@naver.com>
2020-07-02 16:54:40 +0900

    주석 추가

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 var net = require('net')
 var fs = require('fs')
 
+//서버 만듬
 var server = net.createServer(function(socket){
     socket.on('data',function(data){
         var k = ''+data
+        //console.log(k)
         var route = k.split('HTTP/1.1')[0].split(' ')[1]
-        console.log("route:"+ route)
+        //console.log("route:"+ route)
 
        if(route.includes('favicon')) return;
        try {
             output = fs.readFileSync('./public'+route)
+            //console.log(''+output)
             socket.write("HTTP/1.1 200 OK \r\n");
             socket.write("Content-Type: text/html;charset=utf-8\r\n");
             socket.write("Content-Length: " + output.length + "\r\n");
@@ -23,5 +26,6 @@ var server = net.createServer(function(socket){
     })
 })
 
+//서버 킴
 server.listen(8080,function(){
 })
