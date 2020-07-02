ab4cda16f47b0c22deb6ed5093f1d852dad6fc49
woowahan24 <woowahan24@MacBook-Pro.local>
2020-07-02 14:28:41 +0900

    add powerModule

---
+++ powerModule.js
@@ -0,0 +1,10 @@
+function power(base, exponent) {
+    return base**exponent;
+}
+
+function mode(dividend, divisor) {
+    return dividend % divisor;
+}
+
+exports.power = power;
+exports.mode = mode;
