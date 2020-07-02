6fbacfb50dfb793682d901131d92a57f6ac1b82f
nohgijin <nohgijin@naver.com>
2020-07-02 14:20:32 +0900

    곱하기와 나누기

---
+++ index.js
@@ -0,0 +1,4 @@
+var mul=require('./mulmodule')
+
+console.log(mul.divModule(1,2))
+console.log(mul.multiModule(1,2))
---
+++ mulModule.js
@@ -0,0 +1,17 @@
+
+
+// 곱하기
+function multi(a,b){
+return a*b;
+}
+
+// 나누기
+function div(a,b){
+return a/b;
+}
+
+// console.log(multi(1,2));
+// console.log(div(2,2));
+
+module.exports.multiModule=multi;
+module.exports.divModule=div;
